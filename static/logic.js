c10727cf7b555bb06c99ec8cd448c483f148d424
KY <kevinxy00@gmail.com>
2018-03-09 17:24:36 -0500

    geojson data and circle markers

--- static/logic.js
+++ static/logic.js
@@ -7,3 +7,33 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoia2V2aW54eTAwIiwiYSI6ImNqZWJrcDFyczBjZHkycm85bTBtdzNjcjcifQ.5h1SaoW7n6f9YA4mF_dZTA'
 }).addTo(mymap);
+
+// api from earthquake.usgs.gov
+    // all earthquakes from the past week
+var Earthquakes_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(Earthquakes_url, function(data){
+// send the features in the response to the createFeatures function
+createFeatures(data.features);
+
+});
+
+// takes in api data, features object, which is an array of objects, and adds geojson objects to map
+function createFeatures(Earthquakes_data) {
+
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    L.geoJSON(Earthquakes_data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(mymap);
+
+}
